2dddeb687acbf870a139bffa35ef638e8a71eb82
koshika <koshika.gupta@bounceshare.com>
2020-06-21 14:41:55 +0530

    validation on backedn

--- src/node/app.js
+++ src/node/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
-var cors = require("cors")
-
 const app = express();
 
+var cors = require("cors");
 app.use(cors());
 
-app.post('/login', function(req, res){
-    console.log(req.params);
-    
-})
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
 
 app.listen(9000, function (req, res){
     console.log("Node is running and is listening to port: 9000")
 });
+
+/**
+ * used to validation of user.
+ * @param req : request form frontend which has user detail as username and password
+ */
+app.post('/login', function(req, res){
+   if(req.body.username === "admin" && req.body.password === "admin123"){
+        result = {
+            status : 200,
+            messsage : "Authorrized!"
+        } 
+    }
+   result = {
+       status : 403,
+       messsage : "You are not Authorized to visit!"
+   }
+   res.send(result)
+})
